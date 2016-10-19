91cb23aed519fa5d6c915e33b6ac6caaac40b36d
Alexander Chocron <alcho-mac@alexanders-mbp.guest.corp.microsoft.com>
2016-10-19 16:44:04 -0700

    began to make fixes to the api definition for core

--- Microsoft.Sonoma.Core.iOS.Bindings/StructsAndEnums.cs
+++ Microsoft.Sonoma.Core.iOS.Bindings/StructsAndEnums.cs
@@ -1,13 +1,16 @@
 using System;
 using ObjCRuntime;
 
-[Native]
-public enum SNMLogLevel : nuint
+namespace Microsoft.Sonoma.Core.iOS.Bindings
 {
-	None = 0,
-	Assert = 1,
-	Error = 2,
-	Warning = 3,
-	Debug = 4,
-	Verbose = 5
+	[Native]
+	public enum SNMLogLevel : ulong //was given as nuint, but i had to change to compile. not sure what is correct
+	{
+		None = 0,
+		Assert = 1,
+		Error = 2,
+		Warning = 3,
+		Debug = 4,
+		Verbose = 5
+	}
 }
