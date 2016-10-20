aebafaf3ae5c389a25581c72c76181889ec12bac
Alexander Chocron <alcho-mac@alexanders-mbp.guest.corp.microsoft.com>
2016-10-20 09:49:02 -0700

    git being weird

--- Microsoft.Sonoma.Crashes.iOS.Bindings/StructsAndEnums.cs
+++ Microsoft.Sonoma.Crashes.iOS.Bindings/StructsAndEnums.cs
@@ -1,18 +1,22 @@
 using System;
 using ObjCRuntime;
 
-[Native]
-public enum SNMErrorLogSetting : nuint
-{
-	Disabled = 0,
-	AlwaysAsk = 1,
-	AutoSend = 2
-}
+namespace Microsoft.Sonoma.Crashes.iOS.Bindings {
+	//TODO the ulong was actually given as a nuint
+	[Native]
+	public enum SNMErrorLogSetting : ulong
+	{
+		Disabled = 0,
+		AlwaysAsk = 1,
+		AutoSend = 2
+	}
 
-[Native]
-public enum SNMUserConfirmation : nuint
-{
-	DontSend = 0,
-	Send = 1,
-	Always = 2
+	//TODO the ulong was actually given as a nuint
+	[Native]
+	public enum SNMUserConfirmation : ulong
+	{
+		DontSend = 0,
+		Send = 1,
+		Always = 2
+	}
 }
