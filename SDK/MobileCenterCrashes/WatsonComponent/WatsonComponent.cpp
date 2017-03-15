f0e429cd85a5784270468bae49fb809dcb796494
Alexander Chocron <alchocro@microsoft.com>
2017-03-15 14:53:48 -0700

    Add runtime component (broken)

---
+++ SDK/MobileCenterCrashes/WatsonComponent/WatsonComponent.cpp
@@ -0,0 +1,14 @@
+﻿#include "pch.h"
+#include "WatsonComponent.h"
+
+using namespace MobileCenterNative;
+using namespace Platform;
+
+WatsonComponent::WatsonComponent()
+{
+}
+
+void WatsonComponent::Start(Platform::String^ appSecret)
+{
+	WerRegisterCustomMetadata(TEXT("VSMCAppSecret"), const_cast<LPWSTR>(appSecret->Data()));
+}
---
+++ SDK/MobileCenterCrashes/WatsonComponent/WatsonComponent.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+namespace MobileCenterNative
+{
+	public ref class WatsonComponent sealed
+	{
+	public:
+		WatsonComponent();
+		void Start(Platform::String^ appSecret);
+	};
+}
