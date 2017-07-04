c08732bf1dc9cb3e8bd62b3c3aa0fc8f2b1983b4
shulga <a.shulga@samsung.com>
2017-07-04 15:41:39 +0900

    Moving Mobile Center configuration call earlier, before loading GUI
    Signed-off-by: shulga <a.shulga@samsung.com>

--- Tests/Tizen/Contoso.Forms.Test.TizenMobile.cs
+++ Tests/Tizen/Contoso.Forms.Test.TizenMobile.cs
@@ -10,18 +10,9 @@ namespace Contoso.Forms.Test.TizenMobile
         protected override void OnCreate()
         {
             base.OnCreate();
+            MobileCenter.Configure("6825bcfe-3582-4d4a-b3a7-fe06154414a4");
             LoadApplication(new App());
 
-            try
-            {
-                MobileCenter.LogLevel = LogLevel.Verbose;
-                MobileCenterLog.Debug(MobileCenterLog.LogTag, "STARTING MOBILE CENTER API");
-                MobileCenter.Start("6825bcfe-3582-4d4a-b3a7-fe06154414a4", typeof(Analytics), typeof(Crashes));
-            }
-            catch (System.Exception exc)
-            {
-                MobileCenterLog.Debug(MobileCenterLog.LogTag, $"{exc}");
-            }
         }
 
         static void Main(string[] args)
