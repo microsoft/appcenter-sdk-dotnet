c7964244a8e0a1d8721392401198f4c9fc15a59b
Ivan Matkov <v-ivmatk@microsoft.com>
2018-04-09 19:36:28 +0300

    Add service call tests

--- SDK/AppCenter/Microsoft.AppCenter.Windows.Shared/Ingestion/IServiceCall.cs
+++ SDK/AppCenter/Microsoft.AppCenter.Windows.Shared/Ingestion/IServiceCall.cs
@@ -4,13 +4,35 @@ namespace Microsoft.AppCenter.Ingestion
 {
     public interface IServiceCall : IDisposable
     {
+        /// <summary>
+        /// Check if the call is completed due to cancellation.
+        /// </summary>
         bool IsCanceled { get; }
+
+        /// <summary>
+        /// Check if the call is completed.
+        /// </summary>
         bool IsCompleted { get; }
+
+        /// <summary>
+        /// Check if the call is completed due to an unhandled exception.
+        /// </summary>
         bool IsFaulted { get; }
 
+        /// <summary>
+        /// HTTP payload.
+        /// </summary>
         string Result { get; }
+
+        /// <summary>
+        /// The exception thrown from the pipeline.
+        /// </summary>
         Exception Exception { get; }
 
+        /// <summary>
+        /// Handle call results.
+        /// </summary>
+        /// <param name="continuationAction">The action to perform when the call is completed.</param>
         void ContinueWith(Action<IServiceCall> continuationAction);
 
         /// <summary>
