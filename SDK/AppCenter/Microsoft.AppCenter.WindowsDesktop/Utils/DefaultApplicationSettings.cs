19c053a816885e36c633d800b4549c4269b999c7
Alexander Chocron <alchocro@microsoft.com>
2018-07-20 11:51:14 -0700

    Change write location of config

--- SDK/AppCenter/Microsoft.AppCenter.WindowsDesktop/Utils/DefaultApplicationSettings.cs
+++ SDK/AppCenter/Microsoft.AppCenter.WindowsDesktop/Utils/DefaultApplicationSettings.cs
@@ -9,21 +9,24 @@ namespace Microsoft.AppCenter.Utils
 {
     public class DefaultApplicationSettings : IApplicationSettings
     {
-        private readonly object configLock = new object();
-        private IDictionary<string, string> current;
+        private readonly object _configLock = new object();
+        private readonly IDictionary<string, string> _current;
+
+        private static readonly string ConfigPath =
+            Path.Combine(LocalApplicationStorageHelper.LocalApplicationStoragePath, "AppCenter.config");
 
         public DefaultApplicationSettings()
         {
-            current = ReadAll();
+            _current = ReadAll();
         }
         
         public T GetValue<T>(string key, T defaultValue = default(T))
         {
-            lock (configLock)
+            lock (_configLock)
             {
-                if (current.ContainsKey(key))
+                if (_current.ContainsKey(key))
                 {
-                    return (T)TypeDescriptor.GetConverter(typeof(T)).ConvertFromInvariantString(current[key]);
+                    return (T)TypeDescriptor.GetConverter(typeof(T)).ConvertFromInvariantString(_current[key]);
                 }
             }
             SetValue(key, defaultValue);
@@ -33,26 +36,26 @@ namespace Microsoft.AppCenter.Utils
         public void SetValue(string key, object value)
         {
             var invariant = value != null ? TypeDescriptor.GetConverter(value.GetType()).ConvertToInvariantString(value) : null;
-            lock (configLock)
+            lock (_configLock)
             {
-                current[key] = invariant;
+                _current[key] = invariant;
                 SaveValue(key, invariant);
             }
         }
 
         public bool ContainsKey(string key)
         {
-            lock (configLock)
+            lock (_configLock)
             {
-                return current.ContainsKey(key);
+                return _current.ContainsKey(key);
             }
         }
 
         public void Remove(string key)
         {
-            lock (configLock)
+            lock (_configLock)
             {
-                current.Remove(key);
+                _current.Remove(key);
                 var config = OpenConfiguration();
                 config.AppSettings.Settings.Remove(key);
                 config.Save();
@@ -61,7 +64,7 @@ namespace Microsoft.AppCenter.Utils
 
         private void SaveValue(string key, string value)
         {
-            lock (configLock)
+            lock (_configLock)
             {
                 var config = OpenConfiguration();
                 var element = config.AppSettings.Settings[key];
@@ -85,9 +88,7 @@ namespace Microsoft.AppCenter.Utils
 
         private static Configuration OpenConfiguration()
         {
-            var location = Assembly.GetExecutingAssembly().Location;
-            var path = Path.Combine(Path.GetDirectoryName(location), "AppCenter.config");
-            var executionFileMap = new ExeConfigurationFileMap { ExeConfigFilename = path };
+            var executionFileMap = new ExeConfigurationFileMap { ExeConfigFilename = ConfigPath };
             return ConfigurationManager.OpenMappedExeConfiguration(executionFileMap, ConfigurationUserLevel.None);
         }
     }
