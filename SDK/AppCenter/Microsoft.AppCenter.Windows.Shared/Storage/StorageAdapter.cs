09e42f9223d72b6646bd50d84ef407e05a3f159a
Guillaume Perrot <guperrot@microsoft.com>
2019-06-24 15:23:54 -0700

    Add test and fix build

--- SDK/AppCenter/Microsoft.AppCenter.Windows.Shared/Storage/StorageAdapter.cs
+++ SDK/AppCenter/Microsoft.AppCenter.Windows.Shared/Storage/StorageAdapter.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Threading.Tasks;
 using SQLite;
 
--- Tests/Microsoft.AppCenter.Test.Windows/Storage/StorageAdapterTest.cs
+++ Tests/Microsoft.AppCenter.Test.Windows/Storage/StorageAdapterTest.cs
@@ -4,8 +4,10 @@
 using Microsoft.AppCenter.Storage;
 using Microsoft.QualityTools.Testing.Fakes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
 
-namespace Microsoft.AppCenter.Test
+namespace Microsoft.AppCenter.Test.Storage
 {
     [TestClass]
     public class StorageAdapterTest
@@ -13,37 +15,61 @@ namespace Microsoft.AppCenter.Test
         [TestMethod]
         public void CreateStorageAdapterCreatesStorageDirectory()
         {
-            var databaseDirectory = "databaseDirectory";
-            var databasePath = System.IO.Path.Combine(databaseDirectory, "database.db");
-            int callCount = 0;
+            const string databaseDirectory = "databaseDirectory";
+            var databasePath = Path.Combine(databaseDirectory, "database.db");
+            var callCount = 0;
             string actualDirectory = null;
             using (ShimsContext.Create())
             {
-                System.IO.Fakes.ShimDirectory.CreateDirectoryString = directory => {
+                System.IO.Fakes.ShimDirectory.CreateDirectoryString = directory =>
+                {
                     actualDirectory = directory;
                     ++callCount;
-                    return new System.IO.DirectoryInfo(directory);
+                    return new DirectoryInfo(directory);
                 };
                 var _ = new StorageAdapter(databasePath);
-                Assert.AreEqual(1, callCount);
-                Assert.AreEqual(databaseDirectory, actualDirectory);
             }
+            Assert.AreEqual(1, callCount);
+            Assert.AreEqual(databaseDirectory, actualDirectory);
         }
 
         [TestMethod]
         public void CreateStorageAdapterDoesNotCreateDirectoryWhenThereIsNone()
         {
-            var databasePath = "database.db";
-            int callCount = 0;
+            const string databasePath = "database.db";
+            var callCount = 0;
             using (ShimsContext.Create())
             {
-                System.IO.Fakes.ShimDirectory.CreateDirectoryString = path => {
+                System.IO.Fakes.ShimDirectory.CreateDirectoryString = path =>
+                {
                     ++callCount;
-                    return new System.IO.DirectoryInfo(path);
+                    return new DirectoryInfo(path);
                 };
                 var _ = new StorageAdapter(databasePath);
-                Assert.AreEqual(0, callCount);
             }
+            Assert.AreEqual(0, callCount);
+        }
+
+        [TestMethod]
+        public void CreateStorageAdapterExceptionIsWrapped()
+        {
+            const string databaseDirectory = "databaseDirectory";
+            var databasePath = Path.Combine(databaseDirectory, "database.db");
+            Exception actualException = null;
+            using (ShimsContext.Create())
+            {
+                System.IO.Fakes.ShimDirectory.CreateDirectoryString = path => throw new PathTooLongException();
+                try
+                {
+                    var _ = new StorageAdapter(databasePath);
+                }
+                catch (Exception ex)
+                {
+                    actualException = ex;
+                }
+            }
+            Assert.IsInstanceOfType(actualException, typeof(StorageException));
+            Assert.IsInstanceOfType(actualException?.InnerException, typeof(PathTooLongException));
         }
     }
 }
