8a69304ea1aea66349718795c216678ce9cf55e7
Guillaume Perrot <guperrot@microsoft.com>
2019-11-20 17:55:16 -0800

    Try to fix automation

--- Tests/Contoso.Android.FuncTest/MainActivity.cs
+++ Tests/Contoso.Android.FuncTest/MainActivity.cs
@@ -1,8 +1,9 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Threading.Tasks;
 using Android.App;
 using Android.OS;
-using Xunit.Runners.ResultChannels;
+using Android.Util;
 using Xunit.Runners.UI;
 
 namespace Contoso.Android.FuncTest
@@ -14,22 +15,31 @@ namespace Contoso.Android.FuncTest
         protected override void OnCreate(Bundle bundle)
         {
             // tests can be inside the main assembly
-            AddTestAssembly(Assembly.GetExecutingAssembly());	
+            AddTestAssembly(Assembly.GetExecutingAssembly());
 
             //AddTestAssembly(typeof(PortableTests).Assembly);
             // or in any assembly that you load (since JIT is available)
 
-			// you can use the default or set your own custom writer (e.g. save to web site and tweet it ;-)
-			//Writer = new TcpTextWriter ("10.0.1.2", 16384);
-            Task.Run(async () =>
+            try
             {
-                ResultChannel = await TrxResultChannel.CreateTcpTrxResultChannel("10.0.2.2", 16384);
-            });
+#pragma warning disable CS0618 // Type or member is obsolete
+                Writer = new TcpTextWriter("10.0.2.2", 16384);
+#pragma warning restore CS0618 // Type or member is obsolete
+
+                // TODO The new API does not work on command line emulator for some reason (access denied). Need to figure
+                // out why it works when creating an emulator using Android Studio...
+                //ResultChannel = TrxResultChannel.CreateTcpTrxResultChannel("10.0.2.2", 16384).Result;
+            }
+            catch (Exception e)
+            {
+                Log.Warn("AppCenterTest", $"Could not connect to host for reporting results.\n{e}");
+            }
 
             // start running the test suites as soon as the application is loaded
             AutoStart = true;
-			// crash the application (to ensure it's ended) and return to springboard
-			TerminateAfterExecution = true;
+
+            // crash the application (to ensure it's ended) and return to springboard
+            TerminateAfterExecution = true;
 
             // you cannot add more assemblies once calling base
             base.OnCreate(bundle);
