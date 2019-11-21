f43b33b95dfbf1f5c3b5c54bc92b46e38b72312d
Guillaume Perrot <guperrot@microsoft.com>
2019-11-20 18:27:47 -0800

    Don't quit test app on debug

--- Tests/Contoso.Android.FuncTest/MainActivity.cs
+++ Tests/Contoso.Android.FuncTest/MainActivity.cs
@@ -38,8 +38,10 @@ namespace Contoso.Android.FuncTest
             // start running the test suites as soon as the application is loaded
             AutoStart = true;
 
+#if !DEBUG
             // crash the application (to ensure it's ended) and return to springboard
             TerminateAfterExecution = true;
+#endif
 
             // you cannot add more assemblies once calling base
             base.OnCreate(bundle);
