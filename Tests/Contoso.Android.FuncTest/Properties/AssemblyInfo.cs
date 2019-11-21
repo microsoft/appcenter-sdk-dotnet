16780912f91c7f277c93a755d66478bed9eb26a5
Guillaume Perrot <guperrot@microsoft.com>
2019-11-20 18:48:35 -0800

    Some cleanup

--- Tests/Contoso.Android.FuncTest/Properties/AssemblyInfo.cs
+++ Tests/Contoso.Android.FuncTest/Properties/AssemblyInfo.cs
@@ -1,4 +1,7 @@
-﻿using System.Reflection;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using Android.App;
 
@@ -18,7 +21,9 @@ using Android.App;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("2.6.2.0")]
+[assembly: AssemblyInformationalVersion("2.6.2-SNAPSHOT")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
