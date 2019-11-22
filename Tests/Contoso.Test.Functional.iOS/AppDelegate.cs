dc4a5b622aca68328a8b99f11440795ffdbda957
Guillaume Perrot <guperrot@microsoft.com>
2019-11-21 16:31:29 -0800

    Add the iOS functional test project

---
+++ Tests/Contoso.Test.Functional.iOS/AppDelegate.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Reflection;
+using Foundation;
+using UIKit;
+using Xunit.Runner;
+using Xunit.Runners.ResultChannels;
+
+namespace Contoso.Test.Functional.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
+    [Register("AppDelegate")]
+    public class AppDelegate : RunnerAppDelegate
+    {
+        //
+        // This method is invoked when the application has loaded and is ready to run. In this 
+        // method you should instantiate the window, load the UI into it and then make the window
+        // visible.
+        //
+        // You have 17 seconds to return from this method, or iOS will terminate your application.
+        //
+        public override bool FinishedLaunching(UIApplication uiApplication, NSDictionary launchOptions)
+        {
+            // tests can be inside the main assembly
+            AddTestAssembly(Assembly.GetExecutingAssembly());
+            // otherwise you need to ensure that the test assemblies will 
+            // become part of the app bundle
+            //AddTestAssembly(typeof(PortableTests).Assembly);
+
+            // Try to send results to the host via a socket for CI.
+            try
+            {
+                ResultChannel = TrxResultChannel.CreateTcpTrxResultChannel("127.0.0.1", 16384).Result;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"[AppCenterTest] WARN Could not connect to host for reporting results.\n{e}");
+            }
+
+            // start running the test suites as soon as the application is loaded
+            AutoStart = true;
+
+#if !DEBUG
+            // crash the application (to ensure it's ended) and return to springboard
+            TerminateAfterExecution = true;
+#endif
+
+            return base.FinishedLaunching(uiApplication, launchOptions);
+        }
+    }
+}
+
---
+++ Tests/Contoso.Test.Functional.iOS/SomeTest.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Xunit;
+
+namespace Contoso.Test.Functional.iOS
+{
+    public class SomeTest
+    {
+        [Fact]
+        public void MyTest()
+        {
+            Assert.False(false);
+        }
+
+        [Fact]
+        public void MyOtherTest()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
