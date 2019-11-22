d3f53e7a8c8ed8a49cc1cf4821d00f57f734a045
Guillaume Perrot <guperrot@microsoft.com>
2019-11-21 19:17:28 -0800

    Work around the 0 test bug

--- scripts/run-ios-functional-tests.sh
+++ scripts/run-ios-functional-tests.sh
@@ -11,31 +11,37 @@ xcrun simctl boot "${IOS_DEVICE}"
 echo "Installing test app on device..."
 xcrun simctl install "${IOS_DEVICE}" Tests/Contoso.Test.Functional.iOS/bin/iPhoneSimulator/Release/Contoso.Test.Functional.iOS.app
 
-# Workaround for test glitch, if we launch too soon after install it runs 0 test for some reason
-echo "Wait some time to avoid 0 test run glitch..."
-sleep 10
+# We might to run tests multiple times.
+while true
+do
 
-# Listen to tests
-echo "Start listening test results on socket."
-nc -l 127.0.0.1 16384 > results.xml &
-RESULTS=$!
+    # Listen to tests
+    echo "Start listening test results on socket."
+    nc -l 127.0.0.1 16384 > results.xml &
+    RESULTS=$!
 
-# Run tests
-echo "Run test app..."
-xcrun simctl launch "${IOS_DEVICE}" com.contoso.test.functional
+    # Run tests
+    echo "Run test app..."
+    xcrun simctl launch "${IOS_DEVICE}" com.contoso.test.functional
 
-# Wait results
-echo "Waiting test results..."
-wait $RESULTS
+    # Wait results
+    echo "Waiting test results..."
+    wait $RESULTS
 
-# Check if a test failed, also check at least 1 ran as sometimes we get a 0 report...
-echo "Checking test results."
-cat results.xml
-xmllint --xpath "//*[local-name()='Counters'][@passed > 0 and @failed = 0]" results.xml > /dev/null 2>&1
-TEST_RESULT=$?
+    # Check if we ran a test for real
+    echo "Checking test results."
+    cat results.xml
+    if [ "`xmllint --xpath "//*[local-name()='Counters'][@passed = 0]" results.xml 2> /dev/null`" != "" ];
+    then
+        echo -e "\nNo test ran, retrying...".
+        xcrun simctl terminate "${IOS_DEVICE}" com.contoso.test.functional
+    else
+        break
+    fi
+done
 
 # And stop device
 xcrun simctl shutdown "${IOS_DEVICE}"
 
 # Exit with test result code (0 for success, non 0 for failure)
-exit $TEST_RESULT
+xmllint --xpath "//*[local-name()='Counters'][@failed = 0]" results.xml > /dev/null 2>&1
