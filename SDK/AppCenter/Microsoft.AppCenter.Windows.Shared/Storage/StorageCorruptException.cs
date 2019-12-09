8166a1140fd4b8d2c61d5f2abfc9256d9e4c9d0b
Dmitrii Chichuk <v-dmchic@microsoft.com>
2019-12-09 15:18:54 +0400

    Fix bump versions

--- SDK/AppCenter/Microsoft.AppCenter.Windows.Shared/Storage/StorageCorruptException.cs
+++ SDK/AppCenter/Microsoft.AppCenter.Windows.Shared/Storage/StorageCorruptException.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
+
 namespace Microsoft.AppCenter.Storage
 {
     internal class StorageCorruptException : AppCenterException
