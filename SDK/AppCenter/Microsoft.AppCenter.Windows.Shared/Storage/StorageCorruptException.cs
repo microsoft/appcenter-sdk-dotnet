3a27de4e980942e017d2797c592d812911312242
Dmitrii Chichuk <v-dmchic@microsoft.com>
2019-12-09 14:02:22 +0400

    Version fixes

--- SDK/AppCenter/Microsoft.AppCenter.Windows.Shared/Storage/StorageCorruptException.cs
+++ SDK/AppCenter/Microsoft.AppCenter.Windows.Shared/Storage/StorageCorruptException.cs
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
-
 namespace Microsoft.AppCenter.Storage
 {
     internal class StorageCorruptException : AppCenterException
