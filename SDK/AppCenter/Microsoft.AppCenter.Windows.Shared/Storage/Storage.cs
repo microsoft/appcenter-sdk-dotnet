4a0d58592a767c1244e22438f44899af579542d2
Dmitrii Chichuk <v-dmchic@microsoft.com>
2019-12-27 18:41:16 +0400

    Fixed constant - used wrong in a fix by mistake

--- SDK/AppCenter/Microsoft.AppCenter.Windows.Shared/Storage/Storage.cs
+++ SDK/AppCenter/Microsoft.AppCenter.Windows.Shared/Storage/Storage.cs
@@ -51,7 +51,7 @@ namespace Microsoft.AppCenter.Storage
         /// <summary>
         /// Creates an instance of Storage.
         /// </summary>
-        public Storage() : this(DefaultAdapter(), Path.Combine(Constants.AppCenterFilesDirectoryPath, Constants.AppCenterDatabasePath))
+        public Storage() : this(DefaultAdapter(), Path.Combine(Constants.LocalAppData, Constants.AppCenterDatabasePath))
         {
         }
 
