03fd273921101b2367adf8d8e94262b40367d508
Anna Kocheshkova <v-annkoc@microsoft.com>
2019-12-30 16:49:15 +0300

    Add check for http request

--- Tests/Microsoft.AppCenter.Test.Functional/Distribute/DistributeUpdateTest.cs
+++ Tests/Microsoft.AppCenter.Test.Functional/Distribute/DistributeUpdateTest.cs
@@ -10,17 +10,19 @@ namespace Microsoft.AppCenter.Test.Functional.Distribute
     {
         private readonly string _appSecret = Guid.NewGuid().ToString();
 
-        private static string PrefKeyDistributionGroupId = "Distribute.distribution_group_id";
+        private static string DistributionGroupId = "test123";
 
         [Fact]
         public async Task FreshInstallAsync()
         {
             // Reset instance.
             AppCenter.UnsetInstance();
+            var httpNetworkAdapter = new HttpNetworkAdapter();
+            DependencyConfiguration.HttpNetworkAdapter = httpNetworkAdapter;
             // Distribute.UnsetInstance();
 
             // Prepare data.
-            var url = $"appcenter://updates/?request_id={"test"}?distribution_group_id={"test"}?update_token={"test"}?update_setup_failed={"test"}?tester_app_update_setup_failed={"test"}";
+            var url = $"appcenter://updates/?request_id={"test"}?distribution_group_id={DistributionGroupId}?update_token={"test"}?update_setup_failed={"test"}?tester_app_update_setup_failed={"test"}";
 
             // Start App Center.
             AppCenter.LogLevel = LogLevel.Verbose;
@@ -31,10 +33,15 @@ namespace Microsoft.AppCenter.Test.Functional.Distribute
 
             // Open deep link uri.
             Task.Run(() => { Xamarin.Forms.Device.OpenUri(new Uri(url)); }).Wait(1000);
+            // Wait for processing event.
+            await httpNetworkAdapter.HttpResponseTask;
 
+            // Verify. The start session can be in same batch as the event HTTP request so look for it inside.
+            Assert.Equal("GET", httpNetworkAdapter.Method);
+            Assert.Contains(DistributionGroupId, httpNetworkAdapter.Uri);
             // Verify distribution group id value.
-            Assert.True(Xamarin.Forms.Application.Current.Properties.ContainsKey(PrefKeyDistributionGroupId));
-            Assert.Equal(Xamarin.Forms.Application.Current.Properties[PrefKeyDistributionGroupId] as string, "test");
+            //   Assert.True(Xamarin.Forms.Application.Current.Properties.ContainsKey(PrefKeyDistributionGroupId));
+            // Assert.Equal(Xamarin.Forms.Application.Current.Properties[PrefKeyDistributionGroupId] as string, "test");
         }
     }
 }
