990c64c274d9c5c2af75228dee2387eabd0be049
Dmitrii Chichuk <v-dmchic@microsoft.com>
2020-01-10 18:01:38 +0400

    Prepare solution for adding crash functional tests

---
+++ Tests/Microsoft.AppCenter.Test.Functional/Crashes/AppCenterCrashesTest.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.AppCenter.Test.Functional.Crashes
+{
+    using Crashes = Microsoft.AppCenter.Crashes;
+
+    public class AppCenterCrashesTest
+    {
+        private readonly string _appSecret = Guid.NewGuid().ToString();
+
+        public AppCenterCrashesTest()
+        {
+            
+        }
+    }
+}
