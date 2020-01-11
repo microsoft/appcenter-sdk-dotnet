846a3033d499b644af71aceeef323d1f6b35299e
Anna Kocheshkova <v-annkoc@microsoft.com>
2020-01-11 15:49:35 +0300

    Add logic for iOS. Fix android

--- Tests/Microsoft.AppCenter.Test.Functional/Distribute/DistributeUpdateTest.cs
+++ Tests/Microsoft.AppCenter.Test.Functional/Distribute/DistributeUpdateTest.cs
@@ -10,20 +10,25 @@ namespace Microsoft.AppCenter.Test.Functional.Distribute
 
     public class DistributeUpdateTest
     {
-        private readonly string _appSecret = Guid.NewGuid().ToString();
+        private readonly string _appSecret = "e94aaff4-e80d-4fee-9a5f-a84eb6e688fc";
 
-        private static string DistributionGroupId = "test123";
+        private readonly string _installId = Guid.NewGuid().ToString();
+
+        private static string _requestId = "b627efb5-dbf7-4350-92e4-b6ac4dbd09b0";
 
         [Fact]
         public async Task FreshInstallAsync()
         {
             // Prepare data.
+            var prefs = Application.Current.Properties["Distribute.request_id"] = _requestId;
+
             // TODO add url for iOS.
-            var url = "";
+            var androidUrl = $"https://install.portal-server-core-integration.dev.avalanch.es/apps/{_appSecret}/update-setup/?release_hash=adce58cd69799c982a19d427dadc9142772e62ca3462d11a6237493b4d0456ca&redirect_id=com.contoso.test.functional&redirect_scheme=appcenter-{_appSecret}&request_id={_requestId}&platform=android&enable_failure_redirect=true&install_id={_installId}";
+            var iosUrl = $"appcenter-{_appSecret}://?request_id=${_requestId}&distribution_group_id={Guid.NewGuid().ToString()}";
 
             // Setup network adapter.
             // distributionStartSession
-            var httpNetworkAdapter = new HttpNetworkAdapter(expectedLogType: "startSession");
+            var httpNetworkAdapter = new HttpNetworkAdapter(expectedLogType: "distributionStartSession");
             DependencyConfiguration.HttpNetworkAdapter = httpNetworkAdapter;
 
             // Reset instance.
@@ -38,9 +43,13 @@ namespace Microsoft.AppCenter.Test.Functional.Distribute
             // Wait when Distribute wil be start.
             await Distribute.IsEnabledAsync();
 
+            Xamarin.Forms.Device.BeginInvokeOnMainThread(() => {
+                Xamarin.Forms.Device.OpenUri(new Uri(url));
+            });
+
             // Open deep link uri.
-            Task.Run(() => { Xamarin.Forms.Device.OpenUri(new Uri(url)); }).Wait();
-            Task.Delay(5000).Wait();
+            // Task.Run(() => { Xamarin.Forms.Device.OpenUri(new Uri(url)); }).Wait();
+            // Task.Delay(5000).Wait();
 
             // Wait for processing event.
             await httpNetworkAdapter.HttpResponseTask;
