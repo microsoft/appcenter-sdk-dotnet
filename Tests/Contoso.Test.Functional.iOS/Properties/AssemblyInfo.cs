6d4996e9b9be56dde0207875291f06c5626cdd59
Dmitrii Chichuk <v-dmchic@microsoft.com>
2020-01-14 16:49:40 +0400

    Add xunit json config

--- Tests/Contoso.Test.Functional.iOS/Properties/AssemblyInfo.cs
+++ Tests/Contoso.Test.Functional.iOS/Properties/AssemblyInfo.cs
@@ -18,9 +18,6 @@ using Xunit;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Test assemblies
-[assembly: CollectionBehavior(CollectionBehavior.CollectionPerClass, DisableTestParallelization = true)]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
