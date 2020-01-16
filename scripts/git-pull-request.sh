5f78bf854d5b4372f890ea6989ae986e81935af5
Anna Kocheshkova <v-annkoc@microsoft.com>
2020-01-16 16:12:29 +0300

    Fix script

--- scripts/git-pull-request.sh
+++ scripts/git-pull-request.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-resp="$(curl -s -X POST -H 'Content-Type: application/json' -H 'Accept: application/vnd.github.v3+json'  -H 'User-Agent: Microsoft' -H 'Authorization: Token $GithubToken'  -d '{
-      "title": "'Start new ${SDK_NEW_VERSION} release'",
-      "body": "'Start new ${SDK_NEW_VERSION} release'",
+GITHUB_API_URL_TEMPLATE="https://api.github.com/repos/annakocheshkova/appcenter-sdk-dotnet/%s?access_token=%s%s"
+REQUEST_URL_PULL="$(printf $GITHUB_API_URL_TEMPLATE 'pulls' $GithubToken)"
+
+resp="$(curl -s -X POST $REQUEST_URL_PULL -d '{
+      "title": "Start new '${SDK_NEW_VERSION}' version",
+      "body": "Start new '${SDK_NEW_VERSION}' version",
       "head": "test-branch",
       "base": "master"
-    }' https://api.github.com/repos/annakocheshkova/appcenter-sdk-dotnet/pulls)"
-
+    }')"
 url="$(echo $resp | jq -r '.url')"
 
 if [ -z $url ] || [ "$url" == "" ] || [ "$url" == "null" ]; then
@@ -15,3 +17,5 @@ if [ -z $url ] || [ "$url" == "" ] || [ "$url" == "null" ]; then
 else
     echo "A pull request has been created at $url"
 fi
+
+exit 1
