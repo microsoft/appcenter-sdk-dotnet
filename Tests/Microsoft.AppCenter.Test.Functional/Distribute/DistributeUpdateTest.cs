f679bb2d7aabd99e67043b60a725dec6f2a158aa
Ruslan Urmeev <v-ruurme@microsoft.com>
2020-01-27 20:41:09 +0400

    add set update track draft tests

--- Tests/Microsoft.AppCenter.Test.Functional/Distribute/DistributeUpdateTest.cs
+++ Tests/Microsoft.AppCenter.Test.Functional/Distribute/DistributeUpdateTest.cs
@@ -9,6 +9,7 @@ namespace Microsoft.AppCenter.Test.Functional.Distribute
 {
     using Distribute = Microsoft.AppCenter.Distribute.Distribute;
     using AppCenter = Microsoft.AppCenter.AppCenter;
+    using UpdateTrack = Microsoft.AppCenter.Distribute.UpdateTrack;
 
     public enum DistributeTestType
     {
@@ -65,6 +66,70 @@ namespace Microsoft.AppCenter.Test.Functional.Distribute
 
             // Clear.
             DistributeEvent?.Invoke(this, DistributeTestType.Clear);
+        }
+
+        [Fact]
+        public async Task SetUpdateTrackPublicTest()
+        {
+            // Setup network adapter.
+            var httpNetworkAdapter = new HttpNetworkAdapter();
+            DependencyConfiguration.HttpNetworkAdapter = httpNetworkAdapter;
+            var eventTask = httpNetworkAdapter.MockRequest(request => request.Method == "GET");
+            var startServiceTask = httpNetworkAdapter.MockRequestByLogType("startService");
+
+            // Start AppCenter.
+            AppCenter.UnsetInstance();
+            AppCenter.LogLevel = LogLevel.Verbose;
+            Distribute.UpdateTrack = UpdateTrack.Public;
+            AppCenter.Start(Config.ResolveAppSecret(), typeof(Distribute));
+
+            // Wait for "startService" log to be sent.
+            Task.WaitAny(startServiceTask, Task.Delay(5000));
+
+            DistributeEvent?.Invoke(this, DistributeTestType.OnResumeActivity);
+
+            // Wait when Distribute will start.
+            await Distribute.IsEnabledAsync();
+
+            // Wait for processing event.
+            var result = await eventTask;
+
+            // Verify response.
+            Assert.Equal("GET", result.Method);
+            Assert.True(result.Uri.Contains("releases/latest?release_hash"));
+            Assert.True(result.Uri.Contains(Config.ResolveAppSecret()));
+        }
+
+        [Fact]
+        public async Task SetUpdateTrackPrivateTest()
+        {
+            // Setup network adapter.
+            var httpNetworkAdapter = new HttpNetworkAdapter();
+            DependencyConfiguration.HttpNetworkAdapter = httpNetworkAdapter;
+            var eventTask = httpNetworkAdapter.MockRequest(request => request.Method == "GET");
+            var startServiceTask = httpNetworkAdapter.MockRequestByLogType("startService");
+
+            // Start AppCenter.
+            AppCenter.UnsetInstance();
+            AppCenter.LogLevel = LogLevel.Verbose;
+            Distribute.UpdateTrack = UpdateTrack.Private;
+            AppCenter.Start(Config.ResolveAppSecret(), typeof(Distribute));
+
+            // Wait for "startService" log to be sent.
+            Task.WaitAny(startServiceTask, Task.Delay(5000));
+
+            DistributeEvent?.Invoke(this, DistributeTestType.OnResumeActivity);
+
+            // Wait when Distribute will start.
+            await Distribute.IsEnabledAsync();
+
+            // Wait for processing event.
+            var result = await eventTask;
+
+            // Verify response.
+            Assert.Equal("GET", result.Method);
+            Assert.True(result.Uri.Contains("releases/latest?release_hash"));
+            Assert.True(result.Uri.Contains(Config.ResolveAppSecret()));
         }
     }
 }
