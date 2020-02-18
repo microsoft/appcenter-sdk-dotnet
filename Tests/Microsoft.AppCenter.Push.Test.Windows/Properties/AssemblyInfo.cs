f009ddefd38c1c565e18594f7449193b04bd8558
App Center <appcentersdk@microsoft.com>
2020-02-18 12:35:28 +0000

    Start new 3.0.0 release

--- Tests/Microsoft.AppCenter.Push.Test.Windows/Properties/AssemblyInfo.cs
+++ Tests/Microsoft.AppCenter.Push.Test.Windows/Properties/AssemblyInfo.cs
@@ -36,4 +36,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("2.6.5.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
