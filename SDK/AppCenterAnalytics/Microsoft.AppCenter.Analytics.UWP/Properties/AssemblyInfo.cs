cf218867eb71684fc4f3915afa2755690629bf61
App Center <appcentersdk@microsoft.com>
2020-02-20 20:17:31 +0000

    Update SDK version

--- SDK/AppCenterAnalytics/Microsoft.AppCenter.Analytics.UWP/Properties/AssemblyInfo.cs
+++ SDK/AppCenterAnalytics/Microsoft.AppCenter.Analytics.UWP/Properties/AssemblyInfo.cs
@@ -28,6 +28,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyInformationalVersion("3.0.0-SNAPSHOT")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyInformationalVersion("3.0.1-SNAPSHOT")]
+[assembly: AssemblyFileVersion("3.0.1.0")]
 [assembly: ComVisible(false)]
