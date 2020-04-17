f99900d167f573b9717806cfb47b2d8b85ff2fec
App Center <appcentersdk@microsoft.com>
2020-04-17 02:49:14 +0000

    Start new 3.2.0 release

--- Tests/Microsoft.AppCenter.Analytics.Test.Windows/Properties/AssemblyInfo.cs
+++ Tests/Microsoft.AppCenter.Analytics.Test.Windows/Properties/AssemblyInfo.cs
@@ -19,5 +19,5 @@ using System.Runtime.InteropServices;
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("3.1.1.0")]
-[assembly: AssemblyInformationalVersion("3.1.1-SNAPSHOT")]
+[assembly: AssemblyFileVersion("3.2.0.0")]
+[assembly: AssemblyInformationalVersion("3.2.0-SNAPSHOT")]
