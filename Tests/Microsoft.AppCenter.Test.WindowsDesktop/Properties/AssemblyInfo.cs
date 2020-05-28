8d4a5150faa3bbd74c4c74c3da0ebfe10fda5862
App Center <appcentersdk@microsoft.com>
2020-05-28 07:52:06 +0000

    Update SDK version

--- Tests/Microsoft.AppCenter.Test.WindowsDesktop/Properties/AssemblyInfo.cs
+++ Tests/Microsoft.AppCenter.Test.WindowsDesktop/Properties/AssemblyInfo.cs
@@ -20,4 +20,4 @@ using System.Runtime.InteropServices;
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("3.2.2.0")]
+[assembly: AssemblyFileVersion("3.2.3.0")]
