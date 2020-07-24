8eaa437b04b2ac69ffa39e1429a0882818d3ffb4
App Center <appcentersdk@microsoft.com>
2020-07-24 07:37:16 +0000

    Start new 3.4.0 release

--- Tests/Microsoft.AppCenter.NET/Properties/AssemblyInfo.cs
+++ Tests/Microsoft.AppCenter.NET/Properties/AssemblyInfo.cs
@@ -36,8 +36,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("3.3.1.0")]
-[assembly: AssemblyInformationalVersion("3.3.1-SNAPSHOT")]
+[assembly: AssemblyFileVersion("3.4.0.0")]
+[assembly: AssemblyInformationalVersion("3.4.0-SNAPSHOT")]
 
 [assembly: InternalsVisibleTo("Microsoft.AppCenter.Test.Windows")]
 [assembly: InternalsVisibleTo("Microsoft.AppCenter.Analytics.Test.Windows")]
