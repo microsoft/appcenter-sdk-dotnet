356fe9fee70f62c88ee0edcda0ff88e8e11f7a52
App Center <appcentersdk@microsoft.com>
2020-11-13 02:03:08 +0000

    Update Demo apps

--- Apps/Contoso.WinForms.Demo/Properties/AssemblyInfo.cs
+++ Apps/Contoso.WinForms.Demo/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("3.4.3.0")]
-[assembly: AssemblyInformationalVersion("3.4.3")]
+[assembly: AssemblyFileVersion("release/start_4.0.0.0")]
+[assembly: AssemblyInformationalVersion("release/start_4.0.0")]
