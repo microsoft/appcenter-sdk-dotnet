a177d97ab0b8f7e0747a3a9e48ddac8f0aa0e951
App Center <appcentersdk@microsoft.com>
2020-11-19 10:24:43 +0000

    Update SDK version

--- SDK/AppCenterAnalytics/Microsoft.AppCenter.Analytics.iOS/Properties/AssemblyInfo.cs
+++ SDK/AppCenterAnalytics/Microsoft.AppCenter.Analytics.iOS/Properties/AssemblyInfo.cs
@@ -27,6 +27,6 @@ using System.Runtime.CompilerServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.0.0")]
-[assembly: AssemblyInformationalVersion("4.0.0-SNAPSHOT")]
+[assembly: AssemblyFileVersion("4.0.1.0")]
+[assembly: AssemblyInformationalVersion("4.0.1-SNAPSHOT")]
 [assembly: InternalsVisibleTo("Microsoft.AppCenter.Test.Functional")]
--- Tests/Microsoft.AppCenter.Test.WindowsDesktop/Properties/AssemblyInfo.cs
+++ Tests/Microsoft.AppCenter.Test.WindowsDesktop/Properties/AssemblyInfo.cs
@@ -20,4 +20,4 @@ using System.Runtime.InteropServices;
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.1.0")]
