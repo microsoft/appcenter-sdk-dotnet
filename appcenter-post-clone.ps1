fdf448bcb4368863a1bd2a712688cbedcb0f751f
Anastasia Kubova <v-ankubo@microsoft.com>
2021-02-17 10:10:15 +0300

    Update post clone script

--- appcenter-post-clone.ps1
+++ appcenter-post-clone.ps1
@@ -4,4 +4,27 @@
 # Licensed under the MIT License.
 
 echo "Executing post clone script in $pwd"
+
+$nugetFileName = "NuGet.config"
+$contentValue = "<?xml version=""1.0"" encoding=""utf-8""?>`n" +
+"<configuration>`n" +
+"  <packageSources>`n" +
+"    <add key=""nuget"" value=""https://api.nuget.org/v3/index.json"" />`n" +
+"    <add key=""VSTSAC"" value=""https://msmobilecenter.pkgs.visualstudio.com/_packaging/AppCenter/nuget/v3/index.json"" />`n" +
+"  </packageSources>`n" +
+"  <activePackageSource>`n" +
+"    <add key=""All"" value=""(Aggregate source)"" />`n" +
+"  </activePackageSource>`n" +
+"  <packageSourceCredentials>`n" +
+"    <VSTSAC>`n" +
+"      <add key=""Username"" value=""mobilecenter"" />`n" +
+"      <add key=""ClearTextPassword"" value=""NUGET_PASSWORD"" />`n" +
+"    </VSTSAC>`n" +
+"  </packageSourceCredentials>`n" +
+"</configuration>`n"
+
+if (Test-Path $nugetFileName) {
+  Remove-Item $nugetFileName
+}
+New-Item -Path . -Name $nugetFileName -Value $contentValue
 (Get-Content NuGet.config).replace('NUGET_PASSWORD', $env:NUGET_PASSWORD) | Set-Content NuGet.config
