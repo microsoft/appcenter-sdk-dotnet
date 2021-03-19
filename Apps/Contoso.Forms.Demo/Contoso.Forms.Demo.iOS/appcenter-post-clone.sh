9be2a24075e2a8697e6d576f4e21ac7f96fbddd2
Anastasia Kubova <v-ankubo@microsoft.com>
2021-03-19 07:56:25 +0300

    Fix typo

--- Apps/Contoso.Forms.Demo/Contoso.Forms.Demo.iOS/appcenter-post-clone.sh
+++ Apps/Contoso.Forms.Demo/Contoso.Forms.Demo.iOS/appcenter-post-clone.sh
@@ -4,22 +4,22 @@
 #!/usr/bin/env bash
 echo "Executing post clone script in `pwd`"
 
-nugetFileName="NuGet.config"
-contentValue="<?xml version=\""1.0\"" encoding=\""utf-8\""?>\n
- <configuration>\n
-   <packageSources>\n
-     <add key=\""nuget\"" value=\""https://api.nuget.org/v3/index.json\"" />\n
-     <add key=\""VSTSAC\"" value=\""https://msmobilecenter.pkgs.visualstudio.com/_packaging/AppCenter/nuget/v3/index.json\"" />\n
-   </packageSources>\n
-   <activePackageSource>\n
-     <add key=\""All\"" value=\""\('Aggregate source'\)\"" \/>\n
-   </activePackageSource>\n
-   <packageSourceCredentials>\n
-     <VSTSAC>\n
-       <add key=\""Username\"" value=\""mobilecenter\"" />\n
-       <add key=\""ClearTextPassword\"" value=\""$NUGET_PASSWORD\"" />\n
-     </VSTSAC>\n
-   </packageSourceCredentials>\n
+nugetFileName="../../../NuGet.config"
+contentValue="<?xml version=\""1.0\"" encoding=\""utf-8\""?>
+ <configuration>
+   <packageSources>
+     <add key=\""nuget\"" value=\""https://api.nuget.org/v3/index.json\"" />
+     <add key=\""VSTSAC\"" value=\""https://msmobilecenter.pkgs.visualstudio.com/_packaging/AppCenter/nuget/v3/index.json\"" />
+   </packageSources>
+   <activePackageSource>
+     <add key=\""All\"" value=\""\('Aggregate source'\)\"" />
+   </activePackageSource>
+   <packageSourceCredentials>
+     <VSTSAC>
+       <add key=\""Username\"" value=\""mobilecenter\"" />
+       <add key=\""ClearTextPassword\"" value=\""$NUGET_PASSWORD\"" />
+     </VSTSAC>
+   </packageSourceCredentials>
  </configuration>"
 
 if [ -e $nugetFileName ]; then
