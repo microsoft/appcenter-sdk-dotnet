db836c8b8123245f3441a774a22b5df5efede679
Anastasia Kubova <v-ankubo@microsoft.com>
2021-06-23 15:20:50 +0300

    Update post clone script for puppet apps

--- Apps/Contoso.Forms.Puppet/Contoso.Forms.Puppet.iOS/appcenter-post-clone.sh
+++ Apps/Contoso.Forms.Puppet/Contoso.Forms.Puppet.iOS/appcenter-post-clone.sh
@@ -4,6 +4,27 @@
 #!/usr/bin/env bash
 echo "Executing post clone script in `pwd`"
 pushd $APPCENTER_SOURCE_DIRECTORY
-sed -i '' "s/NUGET_PASSWORD/$NUGET_PASSWORD/g" NuGet.config
+nugetFileName="../../../NuGet.config"
+contentValue="<?xml version=\""1.0\"" encoding=\""utf-8\""?>
+ <configuration>
+   <packageSources>
+     <add key=\""nuget\"" value=\""https://api.nuget.org/v3/index.json\"" />
+     <add key=\""VSTSAC\"" value=\""https://msmobilecenter.pkgs.visualstudio.com/_packaging/AppCenter/nuget/v3/index.json\"" />
+   </packageSources>
+   <activePackageSource>
+     <add key=\""All\"" value=\""\('Aggregate source'\)\"" />
+   </activePackageSource>
+   <packageSourceCredentials>
+     <VSTSAC>
+       <add key=\""Username\"" value=\""mobilecenter\"" />
+       <add key=\""ClearTextPassword\"" value=\""$NUGET_PASSWORD\"" />
+     </VSTSAC>
+   </packageSourceCredentials>
+ </configuration>"
+
+if [ -e $nugetFileName ]; then
+    rm $nugetFileName
+fi
+echo $contentValue >> $nugetFileName
 ./build.sh -t=externals-ios
 popd
