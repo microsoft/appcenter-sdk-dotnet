4e38796bb621083e99b10db8ce045f07ad169326
Anastasia Senyushina <v-ankubo@microsoft.com>
2021-07-29 15:54:49 +0300

    Add WinUI test apps

---
+++ Apps/Contoso.WinUI.Desktop.Puppet/Contoso.WinUI.Desktop.Puppet/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace Contoso.WinUI.Desktop.Puppet
+{
+    public class Constants
+    {
+        public static string KeyStorageMaxSize = "StorageMaxSize";
+        public static string KeyUserId = "UserId";
+        public static string KeyCountryCode = "CountryCode";
+        public static string KeyFileErrorAttachments = "FileErrorAttachments";
+        public static string KeyTextErrorAttachments = "TextErrorAttachments";
+    }
+}
