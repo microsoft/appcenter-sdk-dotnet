1fa72c27a9905cf0bd4b4e46da7c682e484886e9
Anastasia Kubova <v-ankubo@microsoft.com>
2021-08-23 15:17:42 +0300

    Fix tests

--- Tests/Microsoft.AppCenter.Test.WindowsDesktop.Shared/Ingestion/Http/HttpNetworkAdapterTest.cs
+++ Tests/Microsoft.AppCenter.Test.WindowsDesktop.Shared/Ingestion/Http/HttpNetworkAdapterTest.cs
@@ -9,25 +9,35 @@ namespace Microsoft.AppCenter.Test.WindowsDesktop.Ingestion.Http
         [Fact]
         public void EnableTls12WhenDisabled()
         {
+#if NET461
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11;
 
             // Init http adapter.
-            new HttpNetworkAdapter();
+            var httpAdapter = new HttpNetworkAdapter();
 
             // Check protocol was added, not the whole value overridden.
             Assert.Equal(SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12, ServicePointManager.SecurityProtocol);
+
+            // Dispose http datapter.
+            httpAdapter.Dispose();
+#endif
         }
 
         [Fact]
         public void EnableTls12WhenAlreadyEnabled()
         {
+#if NET461
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
 
             // Init http adapter.
-            new HttpNetworkAdapter();
+            var httpAdapter = new HttpNetworkAdapter();
 
             // Just check no side effect.
             Assert.Equal(SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12, ServicePointManager.SecurityProtocol);
+
+            // Dispose http datapter.
+            httpAdapter.Dispose();
+#endif
         }
     }
 }
