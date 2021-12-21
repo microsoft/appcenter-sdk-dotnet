5377991ec24b6b4484d7e05b5652f46cf3490bab
App Center <appcentersdk@microsoft.com>
2021-12-21 12:49:25 +0000

    Start new 4.5.0 release

--- SDK/AppCenter/Microsoft.AppCenter.iOS.Bindings/Properties/AssemblyInfo.cs
+++ SDK/AppCenter/Microsoft.AppCenter.iOS.Bindings/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using Foundation;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("4.4.1.0")]
-[assembly: AssemblyInformationalVersion("4.4.1-SNAPSHOT")]
+[assembly: AssemblyFileVersion("4.5.0.0")]
+[assembly: AssemblyInformationalVersion("4.5.0-SNAPSHOT")]
