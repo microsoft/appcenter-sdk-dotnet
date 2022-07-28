c90a8b409b94d6b41ba1a61d2fb2a726b1f74919
Ivan Matkov <matkov.ivan@gmail.com>
2022-07-28 18:44:56 +0200

    Remove .NET Framework 4.6.1 target

--- SDK/AppCenter/Microsoft.AppCenter.WindowsDesktop/Ingestion/Http/HttpNetworkAdapter.cs
+++ SDK/AppCenter/Microsoft.AppCenter.WindowsDesktop/Ingestion/Http/HttpNetworkAdapter.cs
@@ -1,12 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#if NET461
-using System.Net;
-#else
 using System.Net.Http;
 using System.Security.Authentication;
-#endif
 
 namespace Microsoft.AppCenter.Ingestion.Http
 {
@@ -16,19 +12,10 @@ namespace Microsoft.AppCenter.Ingestion.Http
         // Static initializer specific to windows desktop platforms.
         static HttpNetworkAdapter()
         {
-#if NET461
-            // ReSharper disable once InvertIf
-            if ((ServicePointManager.SecurityProtocol & SecurityProtocolType.Tls12) != SecurityProtocolType.Tls12)
-            {
-                ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12;
-                AppCenterLog.Debug(AppCenterLog.LogTag, "Enabled TLS 1.2 explicitly as it was disabled.");
-            }
-#else
             HttpMessageHandlerOverride = () => new HttpClientHandler
             {
                 SslProtocols = SslProtocols.Tls12
             };
-#endif
         }
     }
 }
