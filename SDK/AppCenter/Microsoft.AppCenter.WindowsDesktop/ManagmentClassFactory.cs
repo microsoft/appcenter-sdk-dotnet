b9f77b0686a04840978e834f858776093de51afb
Ivan Matkov <matkov.ivan@gmail.com>
2022-07-28 14:09:41 +0200

    Update .NET 5 to .NET 6 target

--- SDK/AppCenter/Microsoft.AppCenter.WindowsDesktop/ManagmentClassFactory.cs
+++ SDK/AppCenter/Microsoft.AppCenter.WindowsDesktop/ManagmentClassFactory.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 using System.Management;
-using System.Text;
 
 namespace Microsoft.AppCenter
 {
