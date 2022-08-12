0971157af8d0e341906345daad056d8b4ca76773
Ivan Matkov <matkov.ivan@gmail.com>
2022-08-12 14:24:10 +0200

    Revert .NET Framework 4.6.x target

--- SDK/AppCenter/Microsoft.AppCenter.WindowsDesktop/Ingestion/Http/HttpNetworkAdapter.cs
+++ SDK/AppCenter/Microsoft.AppCenter.WindowsDesktop/Ingestion/Http/HttpNetworkAdapter.cs
@@ -1,8 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+#if NET462
+using System.Net;
+#else
 using System.Net.Http;
 using System.Security.Authentication;
+#endif
 
 namespace Microsoft.AppCenter.Ingestion.Http
 {
@@ -12,10 +16,19 @@ namespace Microsoft.AppCenter.Ingestion.Http
         // Static initializer specific to windows desktop platforms.
         static HttpNetworkAdapter()
         {
+#if NET462
+            // ReSharper disable once InvertIf
+            if ((ServicePointManager.SecurityProtocol & SecurityProtocolType.Tls12) != SecurityProtocolType.Tls12)
+            {
+                ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12;
+                AppCenterLog.Debug(AppCenterLog.LogTag, "Enabled TLS 1.2 explicitly as it was disabled.");
+            }
+#else
             HttpMessageHandlerOverride = () => new HttpClientHandler
             {
                 SslProtocols = SslProtocols.Tls12
             };
+#endif
         }
     }
 }
