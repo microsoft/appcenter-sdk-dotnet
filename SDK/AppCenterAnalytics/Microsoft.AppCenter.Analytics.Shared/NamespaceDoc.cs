01a1b323b67890cd3be5509f56b9105d27e0d951
Ivan Matkov <ivan.matkov@akvelon.com>
2022-08-13 14:01:10 +0200

    Remove unsupported shared projects in core and analytics

--- SDK/AppCenterAnalytics/Microsoft.AppCenter.Analytics.Shared/NamespaceDoc.cs
+++ SDK/AppCenterAnalytics/Microsoft.AppCenter.Analytics.Shared/NamespaceDoc.cs
@@ -6,7 +6,7 @@ namespace Microsoft.AppCenter.Analytics
     /// <summary>
     /// Analytics SDK module.
     /// </summary>
-    internal class NamespaceDoc
+    class NamespaceDoc
     {
     }
 }
