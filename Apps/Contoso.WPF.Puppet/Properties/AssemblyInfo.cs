96643131ff1300bc1c60f3a8df93a65c943243da
App Center <appcentersdk@microsoft.com>
2022-11-24 13:14:22 +0000

    Update SDK version

--- Apps/Contoso.WPF.Puppet/Properties/AssemblyInfo.cs
+++ Apps/Contoso.WPF.Puppet/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("5.0.0.0")]
-[assembly: AssemblyInformationalVersion("5.0.0-SNAPSHOT")]
+[assembly: AssemblyFileVersion("5.0.1.0")]
+[assembly: AssemblyInformationalVersion("5.0.1-SNAPSHOT")]
