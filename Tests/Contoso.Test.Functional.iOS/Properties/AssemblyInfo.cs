a9df1b0a0e795aa85a28e4d9bced3bca00873d02
App Center <appcentersdk@microsoft.com>
2023-05-17 11:41:22 +0000

    Start new 5.0.2 release

--- Tests/Contoso.Test.Functional.iOS/Properties/AssemblyInfo.cs
+++ Tests/Contoso.Test.Functional.iOS/Properties/AssemblyInfo.cs
@@ -37,5 +37,5 @@ using Xunit;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("5.0.1.0")]
-[assembly: AssemblyInformationalVersion("5.0.1-SNAPSHOT")]
+[assembly: AssemblyFileVersion("5.0.2.0")]
+[assembly: AssemblyInformationalVersion("5.0.2-SNAPSHOT")]
