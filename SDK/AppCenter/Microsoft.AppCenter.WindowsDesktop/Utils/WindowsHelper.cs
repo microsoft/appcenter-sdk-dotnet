096c46c8923c32578d6502f6a5bf78a997ee77f3
Mikhail Suendukov <v-msuendukov@microsoft.com>
2023-11-16 11:38:12 +0100

    use linq

--- SDK/AppCenter/Microsoft.AppCenter.WindowsDesktop/Utils/WindowsHelper.cs
+++ SDK/AppCenter/Microsoft.AppCenter.WindowsDesktop/Utils/WindowsHelper.cs
@@ -34,47 +34,33 @@ namespace Microsoft.AppCenter.Utils
 
         private static bool _IsRunningAsUwp()
         {
-            try 
+            try
             {
-                // Get the main assembly of the application
                 Assembly mainAssembly = Assembly.GetEntryAssembly();
-
-                // Check if the main assembly references the Microsoft.UI.Xaml or Microsoft.WinUI assembly, which is used by WinUI applications
-                foreach (AssemblyName referencedAssembly in mainAssembly.GetReferencedAssemblies())
-                {
-                    if (referencedAssembly.Name == "Windows.UI.Xaml")
-                    {
-                        return true;
-                    }
-                }
-            } 
-            catch (Exception e) 
+                return mainAssembly.GetReferencedAssemblies()
+                                .Any(referencedAssembly => referencedAssembly.Name == "Windows.UI.Xaml");
+            }
+            catch (Exception e)
             {
                 AppCenterLog.Error(AppCenterLog.LogTag, "Failed to determine whether this application is UWP or not.", e);
             }
 
             return false;
         }
-
         #endregion
 
-        private static bool _IsRunnigAsWinUI()
+        private static bool _IsRunningAsWinUI()
         {
-            try 
+            try
             {
                 // Get the main assembly of the application
                 Assembly mainAssembly = Assembly.GetEntryAssembly();
 
-                // Check if the main assembly references the Microsoft.UI.Xaml or Microsoft.WinUI assembly, which is used by WinUI applications
-                foreach (AssemblyName referencedAssembly in mainAssembly.GetReferencedAssemblies())
-                {
-                    if (referencedAssembly.Name == "Microsoft.UI.Xaml" || referencedAssembly.Name == "Microsoft.WinUI")
-                    {
-                        return true;
-                    }
-                }
-            } 
-            catch (Exception e) 
+                // Check if the main assembly references the Microsoft.UI.Xaml or Microsoft.WinUI assembly using LINQ
+                return mainAssembly.GetReferencedAssemblies()
+                                .Any(referencedAssembly => referencedAssembly.Name == "Microsoft.UI.Xaml" || referencedAssembly.Name == "Microsoft.WinUI");
+            }
+            catch (Exception e)
             {
                 AppCenterLog.Error(AppCenterLog.LogTag, "Failed to determine whether this application is WinUI or not.", e);
             }
@@ -157,7 +143,7 @@ namespace Microsoft.AppCenter.Utils
                 AppCenterLog.Warn(AppCenterLog.LogTag, "Unabled to determine whether this application is WPF or Windows Forms; proceeding as though it is Windows Forms.");
             }
             IsRunningAsUwp = _IsRunningAsUwp();
-            IsRunningAsWinUI = IsRunningAsUwp || _IsRunnigAsWinUI();
+            IsRunningAsWinUI = IsRunningAsUwp || _IsRunningAsWinUI();
         }
 
         // Store the int corresponding to the "Minimized" state for WPF Windows
