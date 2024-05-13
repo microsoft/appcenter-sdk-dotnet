d31b3754060fa4625273e6a8420640d0c6b43043
App Center <appcentersdk@microsoft.com>
2024-05-13 15:28:18 +0000

    Update Demo apps

--- Apps/Contoso.WPF.Demo/Properties/AssemblyInfo.cs
+++ Apps/Contoso.WPF.Demo/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("5.0.3.0")]
-[assembly: AssemblyInformationalVersion("5.0.3")]
+[assembly: AssemblyFileVersion("5.0.4.0")]
+[assembly: AssemblyInformationalVersion("5.0.4")]
