2ff9de35a34c6dea93625f83d2bce9a71c92d153
JiglioNero <mikhail.suendukov@akvelon.com>
2024-08-16 13:08:34 +0200

    improve script

--- scripts/strong-named-sign.cmd
+++ scripts/strong-named-sign.cmd
@@ -13,15 +13,44 @@ setlocal
 set pathToAssemblies=%1
 set privateKey=%2
 
-call "%VSAPPIDDIR%..\Tools\VsDevCmd.bat"
+if not defined VSAPPIDDIR (
+    echo Error: VSAPPIDDIR environment variable is not set.
+    exit /b 1
+)
 
-FOR /D %%d IN (%pathToAssemblies%\*) DO pushd %%d & (FOR %%z IN (*.dll) DO call :signAssemble %%d\%%z) & popd
-EXIT /B
+if not exist "%VSAPPIDDIR%..\Tools\VsDevCmd.bat" (
+    echo Error: VsDevCmd.bat not found at %VSAPPIDDIR%..\Tools\VsDevCmd.bat
+    exit /b 1
+)
+
+call "%VSAPPIDDIR%..\Tools\VsDevCmd.bat"
+if errorlevel 1 (
+    echo Error: Failed to execute VsDevCmd.bat
+    exit /b 1
+)
+
+if not exist "%pathToAssemblies%" (
+    echo Error: The path to assemblies does not exist: %pathToAssemblies%
+    exit /b 1
+)
+
+FOR /D %%d IN (%pathToAssemblies%\*) DO (
+    pushd %%d
+    (FOR %%z IN (*.dll) DO call :signAssemble %%d\%%z)
+    popd
+)
+
+exit /b
 
 :signAssemble
 echo "Start processing the file: %~1"
 if exist %~1 (
     sn -R "%~1" %privateKey%
+    if errorlevel 1 (
+        echo Error: Failed to sign %~1
+        exit /b 1
+    )
 ) else (
-    echo "File %~1 doesn't exist".
+    echo "File %~1 doesn't exist."
+    exit /b 1
 )
