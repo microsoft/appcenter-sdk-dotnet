aacfb0c54189cf72bdfd1cdb83bc7d58b80da6c4
MikhailSuendukov <110986399+MikhailSuendukov@users.noreply.github.com>
2024-08-29 10:30:09 +0200

    Fix strong name signing (#1785)

--- scripts/strong-named-sign.cmd
+++ scripts/strong-named-sign.cmd
@@ -13,15 +13,52 @@ setlocal
 set pathToAssemblies=%1
 set privateKey=%2
 
-call "%VSAPPIDDIR%..\Tools\VsDevCmd.bat"
+if not exist %pathToAssemblies% (
+    echo Error: The path to assemblies does not exist: %pathToAssemblies%
+    exit /b 1
+)
 
-FOR /D %%d IN (%pathToAssemblies%\*) DO pushd %%d & (FOR %%z IN (*.dll) DO call :signAssemble %%d\%%z) & popd
-EXIT /B
+set programFilesVsDevCmdDir=%PROGRAMFILES%\Microsoft Visual Studio\%VSVERSION%\Enterprise\Common7\Tools\VsDevCmd.bat
+set programFilesVsDevCmdDir86=%PROGRAMFILES(x86)%\Microsoft Visual Studio\%VSVERSION%\Enterprise\Common7\Tools\VsDevCmd.bat
+ 
+if exist %programFilesVsDevCmdDir86% (
+    set VSDEVCMDDIR=%programFilesVsDevCmdDir86%
+)
+ 
+if exist %programFilesVsDevCmdDir% (
+    set VSDEVCMDDIR=%programFilesVsDevCmdDir%
+)
+
+if not defined VSDEVCMDDIR (
+    echo Error: VsDevCmd.bat not found for version %VSVERSION%.
+    exit /b 1
+)
+
+echo "Start VS Dev Command CMD from path: %VSDEVCMDDIR%"
+
+call "%VSDEVCMDDIR%"
+if errorlevel 1 (
+    echo Error: Failed to execute VsDevCmd.bat
+    exit /b 1
+)
+
+FOR /D %%d IN (%pathToAssemblies%\*) DO (
+    pushd %%d
+    (FOR %%z IN (*.dll) DO call :signAssemble %%d\%%z)
+    popd
+)
+
+exit /b
 
 :signAssemble
 echo "Start processing the file: %~1"
 if exist %~1 (
     sn -R "%~1" %privateKey%
+    if errorlevel 1 (
+        echo Error: Failed to sign %~1
+        exit /b 1
+    )
 ) else (
-    echo "File %~1 doesn't exist".
+    echo "File %~1 doesn't exist."
+    exit /b 1
 )
